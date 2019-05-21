6bd3ebb02ef4ff9534f3a6abb24eff58952abcde
Jan Mooij <jan.mooij@incentro.com>
2019-05-21 23:16:01 +0200

    adds another exercise

---
+++ courses/day-7-react-basics/src/04-children/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// TODO fix the new Collapsible component to accept & render passed children
+// Add a 'description' to each of your groceries and pass them to the Collapsible component
+// When checking the shopping list item, Collapsible should hide your description
+
+class App extends React.Component {
+
+  render() {
+    return 'foo'
+    // <li>
+    //   <span></span>
+    //   <Collapsible>
+    //     render child here.
+    //   </Collapsible>
+    // </li>
+  }
+}
+
+const Collapsible = () => ('')
+
+export default App
