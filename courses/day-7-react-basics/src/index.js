e90cf1b67ee3f08f3ff4455406819bcfeef69130
Jan Mooij <jan.mooij@incentro.com>
2019-05-22 09:28:52 +0200

    index exercise

--- courses/day-7-react-basics/src/index.js
+++ courses/day-7-react-basics/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './01-my-first-component/solution/App';
+import App from './01-my-first-component/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
