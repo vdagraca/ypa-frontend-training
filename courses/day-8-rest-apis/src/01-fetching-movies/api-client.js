ded93d7a684b972f62ca222cae95c8ad86a9cf58
Vivian da Graca <vivian.da.graca@gmail.com>
2019-09-20 10:26:00 +0200

    searchbar added

--- courses/day-8-rest-apis/src/01-fetching-movies/api-client.js
+++ courses/day-8-rest-apis/src/01-fetching-movies/api-client.js
@@ -20,10 +20,35 @@ export const makeUrl = (endpoint, apiKey, params) => {
 
 // example url: https://api.themoviedb.org/3/discover/movie?api_key=149a733ca73a71f9233c239e0607f92d&sort_by=ascending
 export async function getMovies(sortBy = 'ascending') {
-  const apiUrl = makeUrl()
+  const apiUrl = makeUrl('discover/movie', API_KEY, { sort_by: sortBy })
+
+  const response = await fetch(apiUrl)
+  if (!response.ok) {
+    console.log('failed to fetch')
+    return
+  }
+  return await response.json()
+
 
   // Create the url with the fn above
   // Fetch the data
   // check if the response was OK
   // get the json body and return it
 }
+
+// example url: https://api.themoviedb.org/3/search/movie?api_key=149a733ca73a71f9233c239e0607f92d&query=Avengers
+export async function searchMovies(query) {
+  // construct the correct url
+  const apiUrl = makeUrl('search/movie', API_KEY, { query: query })
+
+  // Fetch movies like above ^
+  const response = await fetch(apiUrl)
+  if (!response.ok) {
+    console.log('failed to fetch')
+    return
+  }
+  console.log(response)
+  return await response.json()
+
+}
+
--- courses/day-8-rest-apis/src/01-fetching-movies/app.js
+++ courses/day-8-rest-apis/src/01-fetching-movies/app.js
@@ -7,21 +7,76 @@ function Container({ children }) {
 }
 
 function List({ children }) {
+
   return <ul className="movie-list">{children}</ul>
 }
 
+
 export function ListItem({ movie }) {
+
+  const [expanded, setExpanded] = useState(false)
+  console.log('expanded', expanded)
+
+  return <li className="list-item" onClick={() => setExpanded(!expanded)}>
+    <h3>{movie.title}</h3>
+    <img className="item-image" src={"https://image.tmdb.org/t/p/w185/" + movie.backdrop_path} alt="movie poster" />
+    <p className={`movie-item${expanded ? '_is_expanded' : ''}`}>
+      {movie.overview}
+    </p>
+  </li>
 }
 
 export function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [query, setQuery] = useState('')
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    async function fetchMovies() {
+      const { results } = await ApiClient.getMovies('popularity.desc')
+      if (results && results.length) {
+        setMovies(results)
+      }
+    }
+    fetchMovies()
+  }, [])
+
+
+
+  // Query  change
+  const searchMovies = async () => {
+    setIsLoading(true)
+
+    const { results } = await ApiClient.searchMovies(query)
+
+    if (results) {
+      setMovies(results)
+    }
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    // TODO
+  }, [query])
 
   return (
-    <Container>
+    <Container className="container">
       <h1>My Movies</h1>
-      <List></List>
+      <div className="seachbar">
+        <form>
+          <input
+            type="text"
+            placeholder="Search movies"
+            onChange={(e) => setQuery(e.target.value)} />
+        </form>
+        <button onClick={() => searchMovies()}>
+          Search
+        </button>
+      </div>
+      <List>
+        {movies.map((movie) => <ListItem key={movie.id} movie={movie}></ListItem>)}
+      </List>
     </Container>
   )
 }
